fc0a26327dc77879556d233386545f886c7b3eb5
Leg0shii <dragonsaremyfavourite@gmail.com>
2024-09-29 13:10:09 +0200

    fix import and add routers

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,7 @@
+from app.database import Base, engine
+from app.routers import auth, documents, search, summaries, tags, users
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
-from .database import Base, engine
-from .routers import documents, tags, users
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
@@ -10,6 +9,9 @@ app = FastAPI()
 app.include_router(users.router)
 app.include_router(documents.router)
 app.include_router(tags.router)
+app.include_router(search.router)
+app.include_router(summaries.router)
+app.include_router(auth.router)
 
 origins = [
     "http://localhost:5000",
